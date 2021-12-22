23bbe3a4ff600a30a9552ee6fa62efe076b00e3d
yangyx91 <yang.yx91@live.cn>
2021-12-22 18:24:36 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "VuePress 云开发网站应用示例",
   description: "基于云开发的 VuePress 网站应用示例",
-  base: "/vuepress/",
+  base: "/",
   themeConfig: {
     nav: [
       { text: "首页", link: "/" },
